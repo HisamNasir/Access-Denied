2fbb3b3f1771017baaa6cc1fef470bc5261753f1
HisamNasir <85251630+HisamNasir@users.noreply.github.com>
2024-03-08 15:02:03 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Sidebar from "./ui/Sidebar";
 
 export default function Home() {
   return (
-    <main className="min-h-screen h-full relative">
-      <div className=" relative pb-[80px] lg:pb-[148px] font px-6 sm:px-12 py-4 md:px-16 md:py-8 lg:px-18 lg:py-10 2xl:px-20 2xl:py-12 sm:space-y-[38px] md:space-y-[48px] lg:space-y-[45px] 2xl:space-y-[70px]">
+    <main>
+      <div className=" relative mb-40 px-6 sm:px-12 py-4 md:px-16 md:py-8 lg:px-18 lg:py-10 2xl:px-20 2xl:py-12 sm:space-y-[38px] md:space-y-[48px] lg:space-y-[45px] 2xl:space-y-[70px]">
         <div className=" sm:flex space-y-6 max-sm:mb-6 justify-between">
           <div className=" max-h-min flex items-center gap-10 md:gap-12 lg:gap-16 xl:gap-20 2xl:gap-[100px]">
             <h1 className=" text-xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-[65px]">
@@ -62,7 +62,7 @@ export default function Home() {
           />
         </div>
       </div>
-      <footer className="flex absolute w-full bottom-0 ">
+      <footer className="flex bottom-0 fixed bg-white w-full">
         <div className="flex mdabsolute bottom-0 -z-50 px-12 py-4 md:px-16 md:py-8 lg:px-18 lg:py-10 2xl:px-20 2xl:py-12 w-full sm:justify-center">
           <Image
             width={46}
